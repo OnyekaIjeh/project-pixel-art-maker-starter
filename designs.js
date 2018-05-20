9d986078c08c9edd053ff7f66971dc1b1bd1d25b
Onyeka Ijeh <onyekaijeh@yahoo.com>
2018-05-20 23:24:34 +0100

    initial commit DONE

--- designs.js
+++ designs.js
@@ -3,8 +3,46 @@
 
 // When size is submitted by the user, call makeGrid()
 
-function makeGrid() {
-
+function makeGrid(row, col, canvas) {
 // Your code goes here!
-
+var grid = "";
+for (var x = 0; x < row; x++){
+  grid += "<tr>";
+  for (var y = 0; y < col; y++){
+    grid += `<td id="${x}${y}"></td>`; //
+    if (y === col - 1){
+      grid += "</tr>";
+    }
+  }
 }
+canvas.html(grid);
+}
+
+function pickColor(){
+  return $('#colorPicker').val();
+}
+
+////////////////////////////////////////////////////////////
+
+var color = pickColor();
+var pixelCanvas = $('#pixelCanvas');
+
+$('#sizePicker').on('submit', function(e){
+  e.preventDefault();
+  color = pickColor();
+  var row = $('#inputHeight').val();
+  var col = $('#inputWeight').val();
+  pixelCanvas = $('#pixelCanvas');
+
+  makeGrid(row, col, pixelCanvas);
+
+});
+
+$('#colorPicker').on('change', function(){
+  color = pickColor();
+});
+
+pixelCanvas.on('click', 'td', function(e){
+  $(e.target).css( "background-color", color );
+
+});
